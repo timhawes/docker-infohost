4033040a3accc66fa1e6c3fa7ced1474c9dfa084
Tim Hawes <me@timhawes.com>
2018-10-27 19:12:54 +0100

    Fix to path matching

--- server.py
+++ server.py
@@ -7,15 +7,19 @@ from flask import Flask, Response, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('<path:subpath>')
-def index(subpath):
+@app.route('/<path:subpath>')
+def multi(subpath):
     data = []
-    data.append('path={}'.format(subpath))
+    data.append('path=/{}'.format(subpath))
     data.append('request.remote_addr={}'.format(request.remote_addr))
     data.append('')
     for k in sorted(os.environ):
         data.append('{}={}'.format(k, os.environ[k]))
     return Response('\n'.join(data) + '\n', mimetype='text/plain')
+
+@app.route('/')
+def index():
+    return multi('')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
