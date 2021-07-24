e3d2951da118e04daca0977f7184f80ce8982c81
Tim Hawes <me@timhawes.com>
2021-07-24 16:14:55 +0100

    Return uname data

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import socket
 
 from flask import Flask, Response, jsonify, request
@@ -8,10 +9,17 @@ app = Flask(__name__)
 
 @app.route('/<path:subpath>')
 def multi(subpath):
+    uname = os.uname()
     data = []
-    data.append('Connection from {}'.format(request.remote_addr))
-    data.append('{} {}'.format(request.method, request.full_path))
+    data.append('sysname={} nodename={} release={} version={}'.format(
+        uname.sysname,
+        uname.nodename,
+        uname.release,
+        uname.version
+    ))
+    data.append('connection from {}'.format(request.remote_addr))
     data.append('')
+    data.append('{} {}'.format(request.method, request.full_path))
     for k, v in request.headers.items():
         data.append('{}: {}'.format(k, v))
     return Response('\n'.join(data) + '\n', mimetype='text/plain')
